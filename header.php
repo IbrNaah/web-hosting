d17b1300daf688e5389612fc22aaba9b0eb4770e
Ibrahim Naah Ahmed <ibrahim.n.ahmed@gmail.com>
2023-08-27 13:49:29 +0500

    php

---
+++ header.php
@@ -0,0 +1,16 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Test</title>
+    <link rel="stylesheet" href="./assets/style.css" />
+
+</head>
+<header>
+    <img id="logo" src="./assets/img/logo.png" alt="logo"/>
+    <nav>
+        <li><a href="./public/cv.html">My projects</a></li>
+        <li><a href="./public/aboutme.html">About me</a></li>
+        <li><a href="./public/cv.html">Contact</a></li>
+    </nav>
+</header>>
+</html>
--- index.php
+++ index.php
@@ -6,14 +6,7 @@
     <link rel="stylesheet" href="./assets/style.css" />
 
 </head>
-<header>
-    <img id="logo" src="./assets/img/logo.png" alt="logo"/>
-    <nav>
-        <li><a href="./public/cv.html">My projects</a></li>
-        <li><a href="./public/aboutme.html">About me</a></li>
-        <li><a href="./public/cv.html">Contact</a></li>
-    </nav>
-</header>>
+<?php include 'header.php'; ?>
 <body>
     <div id="projects">
         <ul>
